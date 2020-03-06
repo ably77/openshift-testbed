c796ff58ba320a6f4c59eb73140c11b58252c756
ably77 <alexbrucely@gmail.com>
2020-03-05 16:40:34 -0800

    Updates

--- tekton/springboot-tekton/runme.sh
+++ tekton/springboot-tekton/runme.sh
@@ -7,7 +7,10 @@ oc create -f projects.yml
 oc project basic-spring-boot-build
 
 # Process and create build and deployment pipelines
-oc process -f build.yml -p APPLICATION_NAME=basic-spring-boot -p NAMESPACE=basic-spring-boot-build -p SOURCE_REPOSITORY_URL="https://github.com/ably77/container-pipelines.git" -p APPLICATION_SOURCE_REPO="https://github.com/ably77/spring-rest.git" | oc apply -f-
+
+# test if you need the static links
+#oc process -f build.yml -p APPLICATION_NAME=basic-spring-boot -p NAMESPACE=basic-spring-boot-build -p SOURCE_REPOSITORY_URL="https://github.com/redhat-cop/container-pipelines" -p APPLICATION_SOURCE_REPO="https://github.com/redhat-cop/spring-rest" | oc apply -f-
+oc process -f build.yml -p APPLICATION_NAME=basic-spring-boot -p NAMESPACE=basic-spring-boot-build | oc apply -f-
 
 oc process -f deployment.yml -p APPLICATION_NAME=basic-spring-boot -p NAMESPACE=basic-spring-boot-build -p SA_NAMESPACE=basic-spring-boot-build -p READINESS_PATH="/health" -p READINESS_RESPONSE="status.:.UP" | oc apply -f-
 
--- tekton/springboot-tekton/uninstall.sh
+++ tekton/springboot-tekton/uninstall.sh
@@ -1,1 +1,5 @@
+oc delete -f github-webhooks/
+
+oc delete -f triggers/
+
 oc delete project basic-spring-boot-build
