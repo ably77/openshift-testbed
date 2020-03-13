d73d553ad027cf1a5485c2b9bd9f8da174b7a4eb
ably77 <alexbrucely@gmail.com>
2020-03-13 11:33:44 -0700

    improvements to multi-cluster

--- multi-cluster/dr/dr_uninstall.sh
+++ multi-cluster/dr/dr_uninstall.sh
@@ -6,5 +6,8 @@ CONTEXT_NAME="dr"
 oc delete -f multi-cluster/${CONTEXT_NAME}/apps/2
 oc delete -f multi-cluster/${CONTEXT_NAME}/apps/1
 
+# delete project
+oc delete -f multi-cluster/${CONTEXT_NAME}/${CONTEXT_NAME}-project.yaml
+
 # delete CRDs
 oc delete -f crds/ --context=${CONTEXT_NAME}
