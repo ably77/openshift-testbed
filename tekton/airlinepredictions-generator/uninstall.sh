996392ab829d3fd74aea5ab81f29761e98eff71f
ably77 <alexbrucely@gmail.com>
2020-03-24 16:12:07 -0700

    Update uninstall.sh

--- tekton/airlinepredictions-generator/uninstall.sh
+++ tekton/airlinepredictions-generator/uninstall.sh
@@ -2,7 +2,7 @@
 
 GITHUB_USERNAME=ably77
 
-oc delete -f fullpipeline.yaml
+#oc delete -f fullpipeline.yaml
 
 oc delete project airlineprediction-generator-dev
 oc delete project airlineprediction-generator-staging
