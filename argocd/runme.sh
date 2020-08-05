c7b4c531d086bf6e2f90741f3c25257d1d85a04a
ably77 <alexbrucely@gmail.com>
2020-08-05 16:40:44 -0700

    add argo repo to CR

--- argocd/runme.sh
+++ argocd/runme.sh
@@ -41,6 +41,3 @@ argocd_server_password=$(oc get secret -n argocd argocd-cluster -o jsonpath='{.d
 argocd_route=$(oc get routes --all-namespaces | grep argocd-server-argocd.apps. | awk '{ print $3 }')
 
 argocd --insecure --grpc-web login ${argocd_route}:443 --username admin --password ${argocd_server_password}
-
-# Add repo to be managed to argo repositories
-argocd repo add ${repo1_url}
