afb5031b2286ada51032cf96d1e0518b39f3a81e
ably77 <alexbrucely@gmail.com>
2020-09-30 15:56:03 -0700

    simplify

--- build-strategies/build-external-registry/docker-secret.sh
+++
@@ -1,5 +0,0 @@
-#docker token
-#7ff3b0fc-d882-4b52-bd18-d5d455201150
-
-# create a dockerhub secret
-oc create secret docker-registry dockerhub --docker-username="ably77" --docker-password="7ff3b0fc-d882-4b52-bd18-d5d455201150" --docker-email="ally@redhat.com" --docker-server="docker.io"
