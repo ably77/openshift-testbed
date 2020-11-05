c0f22cb655b71a001a8a819fd746e669351a227d
ably77 <alexbrucely@gmail.com>
2020-11-05 12:07:15 -0800

    simplify machinesets

--- autoscaler/machinesets/runme.sh
+++ autoscaler/machinesets/runme.sh
@@ -13,7 +13,7 @@ mkdir -p ./generated/${MACHINESET_NAME}
 mkdir -p ./generated/${MACHINESET_NAME}/patches
 
 # modify machineset name
-sed -e "s/<machineset-name>/${MACHINESET_NAME}/g" templates/machineset.yaml > generated/${MACHINESET_NAME}/${MACHINESET_NAME}.yaml
+sed -e "s/<cluster-name>/${CLUSTER_NAME}/g" -e "s/<role>/${ROLE}/g" -e "s/<region>/${REGION}/g" -e "s/<zone>/${ZONE}/g" templates/machineset.yaml > generated/${MACHINESET_NAME}/${MACHINESET_NAME}.yaml
 
 # create kustomization.yaml
 cat <<EOF >./generated/${MACHINESET_NAME}/kustomization.yaml
@@ -40,85 +40,29 @@ commonLabels:
 
 patchesStrategicMerge:
 - patches/aws-template.yaml
-- patches/ebs-storage.yaml
-- patches/instance-spec.yaml
 EOF
 
 # create aws template kustomization patch
 cat <<EOF >./generated/${MACHINESET_NAME}/patches/aws-template.yaml
----
 apiVersion: machine.openshift.io/v1beta1
 kind: MachineSet
 metadata:
-  name: ${MACHINESET_NAME}
-spec:
-  selector:
-    matchLabels:
-      machine.openshift.io/cluster-api-cluster: ${CLUSTER_NAME} 
-      machine.openshift.io/cluster-api-machineset: ${CLUSTER_NAME}-${ROLE}-${REGION}${ZONE} 
-  template:
-    metadata:
-      labels:
-        machine.openshift.io/cluster-api-cluster: ${CLUSTER_NAME} 
-        machine.openshift.io/cluster-api-machine-role: ${ROLE} 
-        machine.openshift.io/cluster-api-machine-type: ${ROLE} 
-        machine.openshift.io/cluster-api-machineset: ${CLUSTER_NAME}-${ROLE}-${REGION}${ZONE} 
-    spec:
-      providerSpec:
-        value:
-          iamInstanceProfile:
-            id: ${CLUSTER_NAME}-worker-profile
-          securityGroups:
-            - filters:
-                - name: tag:Name
-                  values:
-                    - ${CLUSTER_NAME}-worker-sg 
-          subnet:
-            filters:
-              - name: tag:Name
-                values:
-                  - ${CLUSTER_NAME}-private-${REGION}${ZONE} 
-          tags:
-            - name: kubernetes.io/cluster/${CLUSTER_NAME} 
-              value: owned
-EOF
-
-# create instance spec kustomization patch
-cat <<EOF >./generated/${MACHINESET_NAME}/patches/instance-spec.yaml
----
-apiVersion: machine.openshift.io/v1beta1
-kind: MachineSet
-metadata:
-  name: ${MACHINESET_NAME}
+  name: ${CLUSTER_NAME}-${ROLE}-${REGION}${ZONE}
 spec:
   replicas: ${DESIRED_REPLICAS}
   template:
-   providerSpec:
+    spec:
+      providerSpec:
         value:
-          ami: 
+          ami:
             id: ${AMI_ID}
-          instanceType: ${INSTANCE_TYPE}
-          placement:
-            availabilityZone: ${REGION}${ZONE}
-            region: ${REGION}
-EOF
-
-# create storage spec kustomization patch
-cat <<EOF >./generated/${MACHINESET_NAME}/patches/ebs-storage.yaml
----
-apiVersion: machine.openshift.io/v1beta1
-kind: MachineSet
-metadata:
-  name: ${MACHINESET_NAME}
-spec:
-  template:
-   providerSpec:
-        value:
+          apiVersion: awsproviderconfig.openshift.io/v1beta1
           blockDevices:
             - ebs:
                 iops: 0
                 volumeSize: 120
                 volumeType: gp2
+          instanceType: ${INSTANCE_TYPE}
 EOF
 
 # output success
