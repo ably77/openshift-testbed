2fb90eef23d991822859015512387e8b020ce692
ably77 <alexbrucely@gmail.com>
2021-04-05 11:32:07 -0700

    remove unnecessary waitfor

--- scripts/runme.sh
+++ scripts/runme.sh
@@ -3,18 +3,15 @@
 source ./vars.txt
 platform=$1
 
-### Check if argocd CLI is installed
-#ARGOCLI=$(which argocd)
-#echo checking if argocd CLI is installed
-#if [[ $ARGOCLI == "" ]]
-#then
-#        echo
-#        echo "argocd CLI not installed"
-#        echo "see https://github.com/argoproj/argo-cd/blob/master/docs/cli_installation.md for installation instructions"
-#        echo "re-run the script after argocd CLI is installed"
-#        echo
-#        exit 1
-#fi
+if [ $# -eq 0 ]; then
+    echo "No arguments provided. Please provide platform:"
+    echo "Examples:"
+    echo "./scripts/runme.sh aws"
+    echo "./scripts/runme.sh azure"
+    echo "./scripts/runme.sh gcp"
+    echo "./scripts/runme.sh vsphere"
+    exit 1
+fi
 
 # create argocd operator
 echo now deploying argoCD
@@ -37,48 +34,13 @@ oc apply -k https://github.com/${GITHUB_USERNAME}/openshift-testbed-apps/argo-ap
 echo deploying operators
 oc apply -k https://github.com/${GITHUB_USERNAME}/openshift-testbed-apps/argo-apps/operators/meta
 
-### check kafka operator deployment status
-echo waiting for kafka deployment to complete
-./scripts/waitfor-pod -t 10 strimzi-cluster-operator-v0.19.0
-
-### check grafana operator deployment status
-echo checking grafana deployment status before deploying applications
-./scripts/waitfor-pod -t 10 grafana-operator
-
-### check codeready operator deployment status
-echo checking grafana deployment status before deploying applications
-./scripts/waitfor-pod -t 10 codeready-operator
-
-### check openshift pipelines operator deployment status
-echo checking grafana deployment status before deploying applications
-./scripts/waitfor-pod -t 10 openshift-pipelines-operator
-
-### check podium operator deployment status
-echo checking grafana deployment status before deploying applications
-./scripts/waitfor-pod -t 10 podium-operator
-
 ### check kafka deployment status
 echo waiting for kafka deployment to complete
 ./scripts/waitfor-pod -t 20 my-cluster-kafka-2
 
-### check grafana deployment status
-echo checking grafana deployment status before deploying applications
-./scripts/waitfor-pod -t 10 grafana-deployment
-
-### check istio deployment status
-echo checking istio deployment status before deploying applications
-./scripts/wait-for-rollout.sh deployment istiod-basic istio-system 10
-
-### check kiali deployment status
-echo checking kiali deployment status before deploying applications
-./scripts/wait-for-rollout.sh deployment kiali istio-system 10
-
 ### deploy frontend apps in argocd
 echo deploying frontend apps
 oc apply -k https://github.com/${GITHUB_USERNAME}/openshift-testbed-apps/argo-apps/dev/${platform}/meta
-
-### wait for IoT Demo
-./scripts/waitfor-pod -t 10 consumer-app
 
 ### wait for codeready workspace to deploy
 ./scripts/wait-for-rollout.sh deployment codeready codeready 20
@@ -86,4 +48,5 @@ oc apply -k https://github.com/${GITHUB_USERNAME}/openshift-testbed-apps/argo-ap
 ### end
 echo
 echo installation complete
+echo to open demo routes run the script ./scripts/open-routes.sh
 
